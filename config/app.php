d17b98b29beda3e7c1b14f390fc90851d517981a
IceToast <social@icetoast.de>
2022-02-15 12:27:03 +0100

    chore: 🔖 Updated app Version

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ use App\Models\Settings;
 
 return [
 
-    'version' => '0.7.3',
+    'version' => '0.8-DEV',
 
     /*
     |--------------------------------------------------------------------------
@@ -85,7 +85,7 @@ return [
     |
     */
 
-    'locale' =>"en",
+    'locale' => "en",
 
     /*
     |--------------------------------------------------------------------------
@@ -98,7 +98,7 @@ return [
     |
     */
 
-    'available_locales' => array_map('basename', preg_replace('/\\.[^.\\s]{3,4}$/', '', glob(resource_path()."/lang/*.json", GLOB_BRACE))),
+    'available_locales' => array_map('basename', preg_replace('/\\.[^.\\s]{3,4}$/', '', glob(resource_path() . "/lang/*.json", GLOB_BRACE))),
 
 
     /*
